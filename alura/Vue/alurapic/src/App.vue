03baa7f9c88708c9b98e7285eac588d0e776743f
Lucas Nagaoka <Lucas.Nagaoka@ibm.com>
2018-08-27 15:18:35 -0300

    Working with routes and some refactoring/improvements

--- alura/Vue/alurapic/src/App.vue
+++ alura/Vue/alurapic/src/App.vue
@@ -1,61 +1,30 @@
 <template>
 
 <div class="corpo">
+  <meu-menu :rotas="routes"/>
 
-    <h1 class="centralizado">{{ titulo }}</h1>
+  <transition name="pagina">
+    <router-view></router-view>
+  </transition>
+</div>
 
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-        </meu-painel>
-
-      </li>
-    </ul>
-
-  </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
 
 export default {
   components: {
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'meu-menu': Menu
   },
 
   data() {
     return {
-      titulo: "Alurapic",
-      fotos: [],
-      filtro: ''
+      routes
     };
-  },
-
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => (this.fotos = fotos), err => console.log(err));
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -69,16 +38,12 @@ export default {
   width: 96%;
 }
 
-.lista-fotos {
-  list-style: none;
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
 }
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
 </style>
