ceb88a2cb628c57b96ce76291da5de6ad0c71780
Lucas Nagaoka <Lucas.Nagaoka@ibm.com>
2018-08-27 13:47:11 -0300

    Adding filter and img-responsive component

--- alura/Vue/alurapic/src/App.vue
+++ alura/Vue/alurapic/src/App.vue
@@ -4,11 +4,13 @@
 
     <h1 class="centralizado">{{ titulo }}</h1>
 
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
         </meu-painel>
 
       </li>
@@ -19,48 +21,64 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data() {
     return {
       titulo: "Alurapic",
-      fotos: []
+      fotos: [],
+      filtro: ''
     };
   },
+
 
   created() {
     this.$http
       .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
       .then(fotos => (this.fotos = fotos), err => console.log(err));
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
   }
-};
+}
 </script>
 
 <style lang="scss">
-  .centralizado {
-    text-align: center;
-  }
+.centralizado {
+  text-align: center;
+}
 
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    margin: 0 auto;
-    width: 96%;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 
-  .imagem-responsiva {
-    width: 100%;
-  }
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
