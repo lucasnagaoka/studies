04f06be25934404132e5caaea2a46ee5284cba21
Lucas Nagaoka <Lucas.Nagaoka@ibm.com>
2018-10-19 15:01:35 -0300

    Starting part 2. Chapter 1

--- alura/TypeScript/alurabank/app/js/controllers/NegociacaoController.js
+++ alura/TypeScript/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,21 +1,36 @@
-class NegociacaoController {
-    constructor() {
-        this._inputData = $("#data");
-        this._inputValor = $("#valor");
-        this._inputQuantidade = $("#quantidade");
-        this._negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoesView');
-        this.negociacoesView.update(this._negociacoes);
-        this._mensagemView = new MensagemView('#mensagemView');
-    }
-    adiciona(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, '/')), parseInt(this._inputValor.val()), parseFloat(this._inputQuantidade.val()));
-        // this._negociacoes.getArray().forEach(negociacao => {
-        //   console.log(negociacao);
-        // });
-        this._negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this._negociacoes);
-        this._mensagemView.update("Negociação adicionada com sucesso!");
-    }
-}
+System.register(["../models/index", "../views/index"], function (exports_1, context_1) {
+    "use strict";
+    var index_1, index_2, NegociacaoController;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (index_1_1) {
+                index_1 = index_1_1;
+            },
+            function (index_2_1) {
+                index_2 = index_2_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._inputData = $("#data");
+                    this._inputValor = $("#valor");
+                    this._inputQuantidade = $("#quantidade");
+                    this._negociacoes = new index_1.Negociacoes();
+                    this.negociacoesView = new index_2.NegociacoesView('#negociacoesView');
+                    this.negociacoesView.update(this._negociacoes);
+                    this._mensagemView = new index_2.MensagemView('#mensagemView');
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    const negociacao = new index_1.Negociacao(new Date(this._inputData.val().replace(/-/g, '/')), parseInt(this._inputValor.val()), parseFloat(this._inputQuantidade.val()));
+                    this._negociacoes.adiciona(negociacao);
+                    this.negociacoesView.update(this._negociacoes);
+                    this._mensagemView.update("Negociação adicionada com sucesso!");
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
--- alura/TypeScript/alurabank/app/js/models/Negociacao.js
+++ alura/TypeScript/alurabank/app/js/models/Negociacao.js
@@ -1,22 +1,30 @@
-class Negociacao {
-    constructor(data, valor, quantidade) {
-        this._data = data;
-        this._valor = valor;
-        this._quantidade = quantidade;
-    }
-    // o código acima poderia ser escrito da seguinte forma:
-    /*** constructor(private _data: Date, private _valor: number, priate _quantidade: number) {} ***/
-    // ao adicionar "private" aos parämetros do constructor, o TS entende que eles são os atributos da classe e, por conta disso, não é necessário declará-los antes do constructor
-    get data() {
-        return this._data;
-    }
-    get valor() {
-        return this._valor;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get volume() {
-        return this._quantidade * this._valor;
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var Negociacao;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            Negociacao = class Negociacao {
+                constructor(data, valor, quantidade) {
+                    this._data = data;
+                    this._valor = valor;
+                    this._quantidade = quantidade;
+                }
+                get data() {
+                    return this._data;
+                }
+                get valor() {
+                    return this._valor;
+                }
+                get quantidade() {
+                    return this._quantidade;
+                }
+                get volume() {
+                    return this._quantidade * this._valor;
+                }
+            };
+            exports_1("Negociacao", Negociacao);
+        }
+    };
+});
