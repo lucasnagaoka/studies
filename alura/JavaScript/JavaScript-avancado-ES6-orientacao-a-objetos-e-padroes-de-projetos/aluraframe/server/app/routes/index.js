1824cd6b8ff94a83cbec1b71fb766d1448fd6009
Lucas Nagaoka <lucasnagaoka@gmail.com>
2020-07-11 11:29:35 -0300

    Updating dependencies and minor improvements in server

--- alura/JavaScript/JavaScript-avancado-ES6-orientacao-a-objetos-e-padroes-de-projetos/aluraframe/server/app/routes/index.js
+++ alura/JavaScript/JavaScript-avancado-ES6-orientacao-a-objetos-e-padroes-de-projetos/aluraframe/server/app/routes/index.js
@@ -1,18 +1,13 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 
-var api = require('../api');
+let api = require('../api');
 
-module.exports  = function(app) {
-    
-    app.route('/negociacoes/semana')
-        .get(api.listaSemana);
-        
-    app.route('/negociacoes/anterior')
-        .get(api.listaAnterior);
-        
-    app.route('/negociacoes/retrasada')
-        .get(api.listaRetrasada);  
-        
-    app.route('/negociacoes')
-        .post(api.cadastraNegociacao);          
+module.exports = function (app) {
+    app.route('/negociacoes/semana').get(api.listaSemana);
+
+    app.route('/negociacoes/anterior').get(api.listaAnterior);
+
+    app.route('/negociacoes/retrasada').get(api.listaRetrasada);
+
+    app.route('/negociacoes').post(api.cadastraNegociacao);
 };
